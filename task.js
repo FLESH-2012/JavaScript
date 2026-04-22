db92d54b69c9b3f6c74cc13a7c87525e814de385
Shahriyor <shahriyoraminboyev002@gmail.com>
2026-04-22 16:19:33 +0500

    first commit

---
+++ task.js
@@ -0,0 +1,82 @@
+let ism1 = "Shahriyor"
+let familya1 = "Aminboyev"
+
+let ism2 = "Behruz"
+let familya2 = "Ilhomboyev"
+
+let ism3 = "Azizbek"
+let familya3 = "Abdullayev"
+
+let ism4 = "Isokjon"
+let familya4 = "Isakov"
+
+let ism5 = "Ezozbek"
+let familya5 = "Ro'ziboyev"
+
+let ism6 = "Asilbek"
+let familya6= "Ulug'bekov"
+
+let ism7 = "Firdavs"
+let familya7 = "Sadullayev"
+
+let ism8 = "G'ulomjon"
+let familya8 = "Baxtiyorov"
+
+let ism9 = "Sardorbek"
+let familya9 = "Qurbandurdiyev"
+
+let ism10 = "Otajon"
+let familya10 = "Rashidov"
+
+let ism11 = "Bonu"
+let familya11 = "Ilhomboyeva"
+
+let ism12 = "Parizoda"
+let familya12 = "Ilhomboyeva"
+
+let ism13 = "Nafosat"
+let familya13 = "Olimova"
+
+let ism14 = "Sevinchoy"
+let familya14 = "Otanazarova"
+
+let ism15 = "Shodiya"
+let familya15 = "Jumanazarova"
+
+let ism16 = "Maqsuda"
+let familya16 = "Sadullayeva"
+
+let ism17 = "Mehrinoz"
+let familya17 = "Ulug'bekova"
+
+let ism18 = "Mohinur"
+let familya18 = "Jumanazarova"
+
+let ism19 = "Oygul"
+let familya19 = "Rustamova"
+
+
+let ism20 = "Navbahor"
+let familya20 = "Ilhomboyeva"
+
+console.log(ism1 , familya1)
+console.log(ism2 , familya2)
+console.log(ism3 , familya3)
+console.log(ism4, familya4)
+console.log(ism5 , familya5)
+console.log(ism6 , familya6)
+console.log(ism7 , familya7)
+console.log(ism8 , familya8)
+console.log(ism9 , familya9)
+console.log(ism10 , familya10)
+console.log(ism11 , familya1)
+console.log(ism12 , familya1)
+console.log(ism13 , familya1)
+console.log(ism14 , familya1)
+console.log(ism15 , familya1)
+console.log(ism16 , familya1)
+console.log(ism17 , familya1)
+console.log(ism18 , familya1)
+console.log(ism19 , familya1)
+console.log(ism20 , familya1)
+ 
